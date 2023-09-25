523c41292bab9aeaee79e1566e5382b0e41455a8
amijikohir <rihorira.0625@gmail.com>
2023-09-25 13:00:51 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :information, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :lead_time_id, :price, :user, :image).merge(user_id: current_user.id)
+  end
 end
