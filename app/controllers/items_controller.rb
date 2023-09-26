0b557dbb98e224bf1e27c66d2ddcf60bdda72895
amijikohir <rihorira.0625@gmail.com>
2023-09-26 23:25:02 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
-  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -19,6 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -31,5 +43,12 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :information, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :lead_time_id, :price, :user, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
