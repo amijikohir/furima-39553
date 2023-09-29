366fc8adf1b876e396f351108197a40f8188b452
amijikohir <rihorira.0625@gmail.com>
2023-09-29 17:54:07 +0900

    商品情報編集機能③

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new]
   before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, except: [:index, :show, :new]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -43,7 +43,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
