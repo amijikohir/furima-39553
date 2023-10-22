004e20e21bedbeb6f7cc7d0ad44820c58b4b46d1
amijikohir <rihorira.0625@gmail.com>
2023-10-23 00:21:17 +0900

    商品購入機能②

--- db/migrate/20231019151249_add_order_id_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderIdToItems < ActiveRecord::Migration[7.0]
-  def change
-
-  end
-end
--- db/migrate/20231019151640_remove_order_id_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOrderIdFromItems < ActiveRecord::Migration[7.0]
-  def change
-
-  end
-end
